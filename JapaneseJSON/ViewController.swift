1c0c55e4360371b9209b756c68cf7f0279fffdfc
Theo King <tking@Theos-MacBook-Pro.local>
2017-08-04 00:09:20 +0100

    Initial commit

--- JapaneseJSON/ViewController.swift
+++ JapaneseJSON/ViewController.swift
@@ -13,6 +13,39 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let word = "cake"
+        let url = URL(string: "http://jisho.org/api/v1/search/words?keyword=" + word)
+        
+        let task = URLSession.shared.dataTask(with: url!) { (data, response, error) in
+            if error != nil
+            {
+                print("Error")
+            }
+            else
+            {
+                
+                if let content = data
+                {
+                    do
+                    {
+                        let myJson = try JSONSerialization.jsonObject(with: content, options: JSONSerialization.ReadingOptions.mutableContainers) as AnyObject
+                        //print (myJson)
+                        if let jData = myJson["data"] as? [Any]
+                        {
+                            if let japanese = jData[0] as? NSDictionary
+                            {
+                                print(japanese["japanese"])
+                            }
+                            
+                        }
+                    }
+                    catch
+                    {
+                    }
+                }
+            }
+        }
+        task.resume()
     }
 
     override func didReceiveMemoryWarning() {
